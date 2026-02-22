5fd90cfa038a185a1ea95034e5276c9f0b1d9c4d
sshospitalhosting <sshospitalhosting@gmail.com>
2026-02-22 17:17:03 +0530

    wow please work

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/homepage/',
+  // base: '/homepage/',
   plugins: [react()],
   server: {
     proxy: {
